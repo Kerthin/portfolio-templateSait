08fea4198926408b002f006074b8daf8d308629d
Kerthin <belichenko.roden@list.ru>
2021-04-25 23:42:47 +0300

    Project template 'dist' files compilation

---
+++ dist/js/main.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiJdLCJmaWxlIjoibWFpbi5qcyJ9
