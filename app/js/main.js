5dbb4ef166922d81306a28a11f164501abbe36db
Kerthin <belichenko.roden@list.ru>
2021-06-01 18:51:55 +0300

    Connecting the 'anime.js' and 'revealer.js' libraries to create menus

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,16 @@
 //= ../../bower_components/gsap/dist/gsap.min.js
 
 /********************
+	anime.min.js
+********************/
+//= ../../bower_components/anime/anime.min.js
+
+/********************
+	revealer.js
+********************/
+//= ../../bower_components/revealer/revealer.js
+
+/********************
 	blob.js
 ********************/
 //= partials/blob.js
