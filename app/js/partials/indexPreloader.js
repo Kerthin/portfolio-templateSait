91533dadf52322f7c56673c426ac629ba4727604
Kerthin <belichenko.roden@list.ru>
2021-06-23 00:28:10 +0300

    Create components for portfolio page

--- app/js/partials/indexPreloader.js
+++ app/js/partials/indexPreloader.js
@@ -1,55 +1,36 @@
 // Preloader
-TweenMax.to(".preloader", 2, {
-	top: "-100%",
+TweenMax.to(".preloader", 1.6, {
+	top: "-120%",
 	ease: Expo.easeInOut,
-	delay: 9.7
-});
-
-const svgText = anime({
-	targets: '.path',
-	loop: false,
-	direction: 'alternate',
-	strokeDashoffset: [anime.setDashoffset, 0],
-	easing: 'easeInOutSine',
-	duration: 900,
-	delay: (el, i) => { return 1000 + i * 600 }
-});
-
-var tl = new TimelineMax();
-
-tl.from(".preloader__loader", 1.6, {
-	scaleY: "0%",
-	y: 80,
-	ease: Expo.easeInOut,
-	delay: 7.1,
-	transformOrigin: "50% 60%"
-});
-tl.to(".preloader__loader", 1.6, {
-	height: "40vh",
-	scaleY: "0%",
-	ease: Expo.easeInOut,
-	transformOrigin: "0% -60%"
+	delay: .5
 });
 
 // Page
-TweenMax.to(".illustration__curtain", 2, {
-	y: "-100%",
-	ease: Expo.easeInOut,
-	delay: 9.9
-});
-
 var navigationTl = new TweenMax.staggerFrom(".logo, .navigation__links", 2, {
 	opacity: 0,
 	y: 30,
 	ease: Expo.easeInOut,
-	delay: 10.3
+	delay: .6
 }, 0.1);
+
+TweenMax.to(".illustration__curtain", 2, {
+	y: "-100%",
+	ease: Expo.easeInOut,
+	delay: .6
+});
 
 var devLinkTl = new TweenMax.staggerFrom(".social, .mouse, .gmail", 2, {
 	opacity: 0,
 	y: 30,
 	ease: Expo.easeInOut,
-	delay: 11.3
+	delay: .7
+}, 0.1);
+
+var paragraphTl = new TweenMax.staggerFrom(".developerInfo__paragraph, .developerInfo__link", 2, {
+	opacity: 0,
+	y: 30,
+	ease: Expo.easeInOut,
+	delay: .6
 }, 0.1);
 
 // Title
@@ -61,7 +42,7 @@ function titleRise(targetsName) {
 		opacity: [0,1],
 		easing: "easeOutExpo",
 		duration: 2000,
-		delay: (el, i) => { return 10900 + 50 * i }
+		delay: (el, i) => { return 1200 + 50 * i }
 	});
 }
 
@@ -76,10 +57,3 @@ titleRise('.developerInfo__name .letter');
 var textWrapper = document.querySelector('.developerInfo__subtitle');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 titleRise('.developerInfo__subtitle .letter');
-
-var paragraphTl = new TweenMax.staggerFrom(".developerInfo__paragraph, .developerInfo__link", 2, {
-	opacity: 0,
-	y: 30,
-	ease: Expo.easeInOut,
-	delay: 10.5
-}, 0.1);
--- app/js/partials/portfolioPreloader.js
+++ app/js/partials/portfolioPreloader.js
@@ -1,32 +1,75 @@
 // Preloader
-TweenMax.to(".preloader", 2, {
+TweenMax.to(".preloader", 1.5, {
 	top: "-100%",
 	ease: Expo.easeInOut,
-	delay: 10.9
+	delay: .5
 });
 
-const svgText = anime({
-	targets: '.path',
-	loop: false,
-	direction: 'alternate',
-	strokeDashoffset: [anime.setDashoffset, 0],
-	easing: 'easeInOutSine',
-	duration: 900,
-	delay: (el, i) => { return 1000 + i * 600 }
-});
+// const svgText = anime({
+// 	targets: '.path',
+// 	loop: false,
+// 	direction: 'alternate',
+// 	strokeDashoffset: [anime.setDashoffset, 0],
+// 	easing: 'easeInOutSine',
+// 	duration: 600,
+// 	delay: (el, i) => { return 1000 + i * 450 }
+// });
 
-var tl = new TimelineMax();
+// var tl = new TimelineMax();
 
-tl.from(".preloader__loader", 1.6, {
-	scaleY: "0%",
-	y: 80,
+// tl.from(".preloader__loader", 1.6, {
+// 	scaleY: "0%",
+// 	y: 80,
+// 	ease: Expo.easeInOut,
+// 	delay: 8.3,
+// 	transformOrigin: "50% 50%"
+// });
+// tl.to(".preloader__loader", 1.6, {
+// 	height: "40vh",
+// 	scaleY: "0%",
+// 	ease: Expo.easeInOut,
+// 	transformOrigin: "0% -100%"
+// });
+
+// Page
+var navigationTl = new TweenMax.staggerFrom(".logo, .navigation__links", 2, {
+	opacity: 0,
+	y: 30,
 	ease: Expo.easeInOut,
-	delay: 8.3,
-	transformOrigin: "50% 60%"
-});
-tl.to(".preloader__loader", 1.6, {
-	height: "40vh",
-	scaleY: "0%",
+	delay: .6
+}, 0.1);
+
+var portfolioParagraph = new TweenMax.staggerFrom(".portfolioCardContent__paragraph", 2, {
+	opacity: 0,
+	y: 30,
 	ease: Expo.easeInOut,
-	transformOrigin: "0% -60%"
-});
+	delay: .6
+}, 0.1);
+
+var portfolioCardRise = new TweenMax.staggerFrom(".portfolioCardWrap", 2, {
+	opacity: 0,
+	y: 30,
+	ease: Expo.easeInOut,
+	delay: .65
+}, 0.1);
+
+// Title
+function titleRise(targetsName) {
+	anime.timeline().add({
+		targets: targetsName,
+		translateY: [200,0],
+		translateZ: 0,
+		opacity: [0,1],
+		easing: "easeOutExpo",
+		duration: 2000,
+		delay: (el, i) => { return 1200 + 50 * i }
+	});
+}
+
+var textWrapper = document.querySelector('.portfolioCardContent__title');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+titleRise('.portfolioCardContent__title .letter');
+
+var textWrapper = document.querySelector('.portfolioCardContent__paragraphTitle');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+titleRise('.portfolioCardContent__paragraphTitle .letter');
