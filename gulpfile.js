96695acb5cd9c1a514448cc9b8b7452641ac30d5
Kerthin <belichenko.roden@list.ru>
2021-06-23 16:10:26 +0300

    Rename folder 'dist' to 'docs'

--- gulpfile.js
+++ gulpfile.js
@@ -15,12 +15,12 @@ var 	gulp = require('gulp'),
 		reload = browserSync.reload;
 
 var		path 			= {
-			dist: 			{
-				image: 'dist/image/',
-				fonts: 'dist/fonts/',
-				html: 'dist/',
-				css: 'dist/css',
-				js: 'dist/js/'
+			docs: 			{
+				image: 'docs/image/',
+				fonts: 'docs/fonts/',
+				html: 'docs/',
+				css: 'docs/css',
+				js: 'docs/js/'
 			},
 			app:			{
 				image: 'app/image/**/**/*.*',
@@ -39,13 +39,13 @@ var		path 			= {
 				js: 'app/js/*.js'
 			},
 			clean: 			{
-				dist: './dist'
+				docs: './docs'
 			}
 };
 
 var 	config			= {
 		server: {
-			baseDir: "./dist"
+			baseDir: "./docs"
 		},
 		tunnel: true,
 		host: 'localhost',
@@ -53,37 +53,37 @@ var 	config			= {
 		logPrefix: "Frontend_Devil"
 };
 
-gulp.task('html:dist',	function()	{
+gulp.task('html:docs',	function()	{
 	gulp.src(path.app.pug)
 		.pipe(pug({
 			pretty: false
 		}))
-		.pipe(gulp.dest(path.dist.html))
+		.pipe(gulp.dest(path.docs.html))
 		.pipe(reload({stream: true}));	//Перезагрузка сервера для обновлений
 });
 
-gulp.task('js:dist',	function()	{
+gulp.task('js:docs',	function()	{
 	gulp.src(path.app.js)
 		.pipe(rigger())
 		.pipe(sourcemaps.init())
 		.pipe(uglify())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(path.dist.js))
+		.pipe(gulp.dest(path.docs.js))
 		.pipe(reload({stream:	true}));
 });
 
-gulp.task('style:dist',	function()	{
+gulp.task('style:docs',	function()	{
 	gulp.src(path.app.scss)
 		.pipe(sourcemaps.init())
 		.pipe(sass())
 		.pipe(prefixer())
 		.pipe(cssmin())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(path.dist.css))
+		.pipe(gulp.dest(path.docs.css))
 		.pipe(reload({stream: true}));
 });
 
-gulp.task('image:dist',	function()	{
+gulp.task('image:docs',	function()	{
 	gulp.src(path.app.image)
 		.pipe(imagemin({
 			progressive: true,
@@ -91,52 +91,52 @@ gulp.task('image:dist',	function()	{
 			use: [pngquant()],
 			interlaced: true 
 		}))
-		.pipe(gulp.dest(path.dist.image))
+		.pipe(gulp.dest(path.docs.image))
 		.pipe(reload({stream: true}));
 });
 
-gulp.task('fonts:dist',	function()	{
+gulp.task('fonts:docs',	function()	{
 	gulp.src(path.app.fonts)
-		.pipe(gulp.dest(path.dist.fonts))
+		.pipe(gulp.dest(path.docs.fonts))
 });
 
-gulp.task('dist',	[
-	'html:dist',
-	'style:dist',
-	'js:dist',
-	'image:dist',
-	'fonts:dist'
+gulp.task('docs',	[
+	'html:docs',
+	'style:docs',
+	'js:docs',
+	'image:docs',
+	'fonts:docs'
 ]);
 
 gulp.task('watch',	function()	{
 	watch([path.watch.pug],	function(event,	cb)	{
-		gulp.start('html:dist');
+		gulp.start('html:docs');
 	});
 	watch([path.watch.scss],	function(event,	cb)	{
-		gulp.start('style:dist');
+		gulp.start('style:docs');
 	});
 	watch([path.watch.js],		function(event,	cb)	{
-		gulp.start('js:dist');
+		gulp.start('js:docs');
 	});
 	watch([path.watch.image],	function(event,	cb)	{
-		gulp.start('image:dist');
+		gulp.start('image:docs');
 	});
 	watch([path.watch.fonts],	function(event,	cb)	{
-		gulp.start('fonts:dist');
+		gulp.start('fonts:docs');
 	});
 });
 
 gulp.task('webserver',	function()	{
 	browserSync({
      server: {
-        baseDir: "dist/",
+        baseDir: "docs/",
         directory: true
      }
    });
 });
 
 gulp.task('clean',		function(cb)	{
-	rimraf(path.clean.dist, cb);
+	rimraf(path.clean.docs, cb);
 });
 
-gulp.task('default',	['dist',	'webserver',	'watch']);
+gulp.task('default',	['docs',	'webserver',	'watch']);
