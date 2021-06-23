934c5f70a900c53314b9233fb2ea95c0368813dd
Kerthin <belichenko.roden@list.ru>
2021-06-23 15:14:18 +0300

    Create change class script for burger menu

--- app/js/partials/popup.js
+++ app/js/partials/popup.js
@@ -4,31 +4,36 @@
 			revealer = new RevealFx(popupMenu),
 			burger = document.querySelector('.burgerWrap');
 
-		burger.onclick = function() {
-			if (burger.classList.contains('js-burgerWrap_close') === true) {
-				revealer.reveal({
-					bgcolor: '#f7e0b5',
-					direction: 'tb',
-					duration: 700,
-					onCover: function(contentEl, revealerEl) {
-						popupMenu.classList.add('js-popupMenu_open');
-						contentEl.style.opacity = 1;
-						burger.classList.remove('js-burgerWrap_close');
-					}
-				});
-			} else {
-				popupMenu.classList.remove('js-popupMenu_open');
-				revealer.reveal({
-					bgcolor: '#f7e0b5',
-					direction: 'bt',
-					duration: 700,
-					onCover: function(contentEl, revealerEl) {
-						popupMenu.classList.remove('js-popupMenu_open');
-						contentEl.style.opacity = 0;
-						burger.classList.add('js-burgerWrap_close');
-					}
-				});
-			}
+	burger.onclick = function() {
+		if (burger.classList.contains('js-burgerWrap_close') === true) {
+
+			burger.classList.remove('js-burgerWrap_close');
+			burger.classList.add('js-burgerWrap_open');
+
+			revealer.reveal({
+				bgcolor: '#f7e0b5',
+				direction: 'tb',
+				duration: 700,
+				onCover: function(contentEl, revealerEl) {
+					popupMenu.classList.add('js-popupMenu_open');
+					contentEl.style.opacity = 1;
+				}
+			});
+		} else {
+
+			burger.classList.add('js-burgerWrap_close');
+			burger.classList.remove('js-burgerWrap_open');
+
+			revealer.reveal({
+				bgcolor: '#f7e0b5',
+				direction: 'bt',
+				duration: 700,
+				onCover: function(contentEl, revealerEl) {
+					popupMenu.classList.remove('js-popupMenu_open');
+					contentEl.style.opacity = 0;
+				}
+			});
 		}
+	}
 	popupMenu.addEventListener('submit', function(ev) {ev.preventDefault();});
 })();
