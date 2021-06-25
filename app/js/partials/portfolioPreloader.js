2654d7b8ccb918f84855b443061ae17ace73c080
Kerthin <belichenko.roden@list.ru>
2021-06-25 17:39:13 +0300

    Smaller subtitle on the home page for all screens

--- app/js/partials/portfolioPreloader.js
+++ app/js/partials/portfolioPreloader.js
@@ -5,32 +5,6 @@ TweenMax.to(".preloader", 1.6, {
 	delay: .5
 });
 
-// const svgText = anime({
-// 	targets: '.path',
-// 	loop: false,
-// 	direction: 'alternate',
-// 	strokeDashoffset: [anime.setDashoffset, 0],
-// 	easing: 'easeInOutSine',
-// 	duration: 600,
-// 	delay: (el, i) => { return 1000 + i * 450 }
-// });
-
-// var tl = new TimelineMax();
-
-// tl.from(".preloader__loader", 1.6, {
-// 	scaleY: "0%",
-// 	y: 80,
-// 	ease: Expo.easeInOut,
-// 	delay: 8.3,
-// 	transformOrigin: "50% 50%"
-// });
-// tl.to(".preloader__loader", 1.6, {
-// 	height: "40vh",
-// 	scaleY: "0%",
-// 	ease: Expo.easeInOut,
-// 	transformOrigin: "0% -100%"
-// });
-
 // Page
 var navigationTl = new TweenMax.staggerFrom(".logo, .navigation__links", 2, {
 	opacity: 0,
